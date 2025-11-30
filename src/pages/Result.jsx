402fdde6de80a9dc06782b8a890bedc156ca81c5
Souvik Chatterjee <souvikchatterjee@Souviks-MacBook-Air.local>
2025-11-30 20:06:00 +0530

    Result page made

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,10 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { assets } from "../assets/assets/assets.js";
 
 const Result = () => {
+  const [image, setImage] = useState(assets.sample_img_1);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [input, setInput] = useState("");
+
+  const onSubmitHandler = async (e) => {};
   return (
-    <div>
-      <h1>Result Page</h1>
-    </div>
+    <form
+      className="flex flex-col min-h-[90vh] justify-center items-center"
+      onSubmit={onSubmitHandler}
+    >
+      <div>
+        <div className="relative ">
+          <img src={image} alt="" className="max-w-sm rounded " />
+          <span
+            className={`absolute bottom-0 left-0 h-1 bg-blue-500  ${
+              isLoading ? "w-full transition-all duration-[10s]" : "w-0"
+            }`}
+          />
+        </div>
+        <p className={!isLoading ? "hidden" : ""}>Loading...</p>
+      </div>
+
+      {!isImageLoaded && (
+        <div className="flex items-center w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Describe what you want to generate"
+            className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+
+          <button
+            type="submit"
+            className="text-white bg-zinc-900 px-10 sm:px-16 py-3 rounded-full cursor-pointer"
+          >
+            Generate
+          </button>
+        </div>
+      )}
+
+      {isImageLoaded && (
+        <div className="flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 rounded-full mt-10">
+          <p
+            className="bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer"
+            onClick={() => setIsImageLoaded(false)}
+          >
+            Generate Another
+          </p>
+          <a
+            href={image}
+            download
+            className="bg-zinc-900 px-10 py-3 cursor-pointer rounded-full"
+          >
+            Download
+          </a>
+        </div>
+      )}
+    </form>
   );
 };
 
